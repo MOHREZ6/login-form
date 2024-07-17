08d795c1b8b1e0ae9a5d4b1cb12d701e26a85e06
mohammad.ab95 <mohammadreza.ab1895@gmail.com>
2024-07-18 00:17:11 +0330

    add:react hook form #1

--- components/Login.js
+++ components/Login.js
@@ -1,9 +1,22 @@
+import { useForm } from "react-hook-form";
+
 import Image from "next/image";
 import loginimage from "../public/images/loginimage.jpg";
 
 function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+
+  function submitHandler({ email, password }) {}
+
   return (
-    <section className=" flex  w-2/4   w-full	  h-screen	bg-slate-200	">
+    <form className=" flex w-2/4 w-full h-screen bg-slate-200	" 
+    onSubmit={handleSubmit(submitHandler)}>
+
       <div className="flex  w-full justify-center		  m-auto	">
         <div className="w-7/12 	">
           <p className="text-6xl 	">Login form</p>
@@ -14,14 +27,24 @@ function Login() {
           </p>
           <p className="text-xl	my-3.5">Username</p>
           <input
+          {...register('email', {required:true})}
+          type="email"
             placeholder="your-email@gmail.com"
             className=" w-full	py-3 mt-1 border-2   p-4 focus:border-transparent focus:outline-none	rounded	 pl-2 "
-          ></input>
+          /> {errors.email && (<div className="text-red-500	">Please enter email</div>)}
           <p className="text-xl my-3.5">Password</p>
           <input
+          {...register('password', {required:true,
+            minLength:{
+              value:7,message:'password must be at least 7 characters'
+               
+            }
+          })}
+          type="password"
             placeholder="Password"
             className="w-full	py-3 mt-1 border-2   p-4 focus:border-transparent focus:outline-none		rounded pl-2		"
           ></input>
+          {errors.password && (<div>{errors.password.message}</div>)}
           <div className=" flex justify-between		">
             <div className="my-3.5">
               <input
@@ -44,7 +67,6 @@ function Login() {
             </span>
           </div>
           <button
-            type="button"
             className="inline-block w-full rounded bg-orange-500	 px-7 pb-2 pt-3  text-sm font-medium uppercase leading-normal text-white  transition duration-150 ease-in-out  hover:bg-orange-400	dark:shadow-black/30 dark:hover:shadow-dark-strong  "
           >
             Login
@@ -54,7 +76,7 @@ function Login() {
       <div className="flex w-full  bg-teal-300	 	">
         <Image src={loginimage} alt="flower" width={800} height={900} />
       </div>
-    </section>
+    </form>
   );
 }
 
